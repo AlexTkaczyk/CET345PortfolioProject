17afab5ea7753ce53c11710158409252ac138c09
Alex <kiel181@wp.pl>
2021-03-12 13:50:53 +0000

    Background + Parallax Scrolling script

---
+++ Assets/Scripts/Parallax_scroll.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parallax_scroll : MonoBehaviour
+{
+    private float sprite_length, start_pos;
+    public GameObject m_camera;
+    public float m_parallax;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        start_pos = transform.position.x;
+        sprite_length = GetComponent<SpriteRenderer>().bounds.size.x;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float temp = (m_camera.transform.position.x * (1 - m_parallax));
+        float m_distance = (m_camera.transform.position.x * m_parallax);
+
+        transform.position = new Vector3(start_pos + m_distance, transform.position.y, transform.position.z);
+    
+        if(temp > start_pos + sprite_length)
+        {
+            start_pos += sprite_length;
+        }
+        else if (temp <start_pos - sprite_length)
+        {
+            start_pos -= sprite_length;
+        }
+    
+    }
+}
