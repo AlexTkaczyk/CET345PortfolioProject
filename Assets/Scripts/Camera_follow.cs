50d41c620547e1c14cad227e1979311c973a06ea
Alex <kiel181@wp.pl>
2021-03-12 16:24:48 +0000

    Camera follow script

---
+++ Assets/Scripts/Camera_follow.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera_follow : MonoBehaviour
+{
+    public GameObject m_character;
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(m_character.transform.position.x, m_character.transform.position.y, transform.position.z);
+
+    }
+}
