ccebcb01e920de6c34acd14627b523cdc7e67e86
Alex <kiel181@wp.pl>
2021-05-29 12:32:41 +0100

    Enemies
    Enemy scriptable objects

---
+++ Assets/Scripts/Enemy_scriptableObject.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Enemy", menuName = "Enemies/Enemy")]
+public class Enemy_scriptableObject : ScriptableObject
+{
+    public string enemy_name = "New Enemy";
+    public int enemy_health;
+}
