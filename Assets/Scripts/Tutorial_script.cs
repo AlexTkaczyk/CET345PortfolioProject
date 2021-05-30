0a27d2f750031786276cfed758b0ad613b187cd8
Alex <kiel181@wp.pl>
2021-05-30 13:53:59 +0100

    Tutorial screen sounds and transitions

--- Assets/Scripts/Tutorial_script.cs
+++ Assets/Scripts/Tutorial_script.cs
@@ -11,22 +11,9 @@ public class Tutorial_script : MonoBehaviour
     public TMP_InputField player_name_input;
     public Data_scriptableObject player_name_data;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-    public void Start_Game()
+    public void Save_Name()
     {
         Debug.Log("Player: " + player_name_input.text);
         player_name_data.data_string = player_name_input.text;
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 }
