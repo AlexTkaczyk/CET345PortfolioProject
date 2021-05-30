e799ae1ab663f8189990469b8ea55df2c1a343e7
Alex <kiel181@wp.pl>
2021-05-30 12:30:50 +0100

    Animations and scriptable objects changes

--- Assets/Scripts/Ability_scriptableObject.cs
+++ Assets/Scripts/Ability_scriptableObject.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 public class Ability_scriptableObject : ScriptableObject
 {
     public string ability_name = "New Ability";
+    public int ability_damage;
 
     public virtual void Use()
     {
--- Assets/Scripts/UI_script.cs
+++ Assets/Scripts/UI_script.cs
@@ -7,8 +7,8 @@ using TMPro;
 public class UI_script : MonoBehaviour
 {
     public TextMeshProUGUI player_name;
-    Tutorial_script tutorial;
     public Data_scriptableObject player_name_data;
+
     // Start is called before the first frame update
     void Start()
     {
