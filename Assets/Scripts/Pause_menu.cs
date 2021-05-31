89f1e2b7ff31a80c7b3e350f0d9d89ad3b1caef7
Alex <kiel181@wp.pl>
2021-05-31 17:08:38 +0100

    Pause menu

---
+++ Assets/Scripts/Pause_menu.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pause_menu : MonoBehaviour
+{
+    public GameObject pause_menu;
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            pause_menu.SetActive(true);
+        }
+    }
+
+    public void Resume()
+    {
+        pause_menu.SetActive(false);
+    }
+
+    public void Quit_game()
+    {
+        Application.Quit();
+    }
+}
